27c300d1c95b21e38910468803101cfdafe30b6f
dYoshitakaKato <phoenix1331019@gmail.com>
2017-05-10 00:26:02 +0900

    [Fix] Findでの取得に変更

--- LevelGraph/KanmusuDbUtil.cs
+++ LevelGraph/KanmusuDbUtil.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using Grabacr07.KanColleWrapper;
 using Grabacr07.KanColleWrapper.Models;
+using System.Data.Entity;
 
 namespace LevelGraph
 {
@@ -33,33 +34,32 @@ namespace LevelGraph
 
         public static void insertLevel(Fleet fleet)
         {
-            foreach (Ship ship in fleet.Ships)
+            using (LevelLogDBContext context = new LevelLogDBContext())
             {
                 Models.LevelLog levelLog;
-                using (LevelLogDBContext context = new LevelLogDBContext())
+                foreach (Ship ship in fleet.Ships)
                 {
                     if (ship.IsLocked)
                     {
-                        levelLog = new Models.LevelLog();
-                        Models.LevelLog result;
-                        try
+                        levelLog = context.LevelLogs.Find(ship.Id, DateTime.Now.Date);
+                        //.SqlQuery<Models.LevelLog>("SELECT * FROM LevelLog WHERE Id = {0} AND InsertDate = {1}",
+                        //ship.Id, DateTime.Now.ToString("yyyy/MM/dd")).FirstAsync().Result;
+                        if (null == levelLog)
                         {
-                            result = context.Database.SqlQuery<Models.LevelLog>("SELECT * FROM LevelLog WHERE Id = {0} AND InsertDate = {1}",
-                            ship.Id, DateTime.Now.ToString("yyyy/MM/dd")).FirstAsync().Result;
-                            result.Level = ship.Level;
-                        }
-                        catch (AggregateException e)
-                        {
-                            result = new Models.LevelLog();
+                            levelLog = new Models.LevelLog();
                             levelLog.Id = ship.Id;
                             levelLog.ShipId = ship.Info.Id;
-                            levelLog.InsertDate = DateTime.Now;
+                            levelLog.InsertDate = DateTime.Now.Date;
                             levelLog.Level = ship.Level;
                             context.LevelLogs.Add(levelLog);
-                        }
-                        context.SaveChanges();
+                        }  else
+                        {
+                            levelLog.Level = ship.Level;
+                            levelLog.ShipId = ship.Info.Id;
+                        } 
                     }
                 }
+                context.SaveChanges();
             }
         }
 
